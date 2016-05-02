068b9fb69480340f88131b5e1e30a8ba6288de1e
borova <borovadev01@gmail.com>
2016-05-02 16:37:16 +0200

    14: JSF messages & growl

--- sitemonitoring-tutorial/src/main/java/info/borova/sitemonitoring/controller/CheckListController.java
+++ sitemonitoring-tutorial/src/main/java/info/borova/sitemonitoring/controller/CheckListController.java
@@ -7,9 +7,11 @@ import java.io.Serializable;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -38,5 +40,6 @@ public class CheckListController implements Serializable {
 		checkService.save(check);
 		checks = checkService.findAll();
 		check = new Check();
+		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Check saved!", null));
 	}
 }
