5d76e3374b2b156799e8c55d2910aede9a9e28ba
borova <borovadev01@gmail.com>
2016-05-02 16:08:08 +0200

    JSF bean scopes

--- sitemonitoring-tutorial/src/main/java/info/borova/sitemonitoring/controller/CheckListController.java
+++ sitemonitoring-tutorial/src/main/java/info/borova/sitemonitoring/controller/CheckListController.java
@@ -3,11 +3,13 @@ package info.borova.sitemonitoring.controller;
 import info.borova.sitemonitoring.entity.Check;
 import info.borova.sitemonitoring.service.CheckService;
 
+import java.io.Serializable;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.ViewScoped;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -15,7 +17,10 @@ import lombok.Setter;
 @Setter
 @Getter
 @ManagedBean
-public class CheckListController {
+@ViewScoped
+public class CheckListController implements Serializable {
+
+	private static final long serialVersionUID = 1L;
 
 	@ManagedProperty("#{checkService}")
 	private CheckService checkService;
@@ -31,5 +36,6 @@ public class CheckListController {
 
 	public void save() {
 		checkService.save(check);
+		check = new Check();
 	}
 }
